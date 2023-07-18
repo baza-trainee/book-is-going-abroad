b24db27ee1de7580d3f528592b71cccce56d4953
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-07-18 08:08:32 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
+    },
+    extends: [
+        'prettier',
+        'airbnb-base',
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        'linebreak-style': ['error', 'windows'],
+    },
+    ignorePatterns: ['.eslintrc.js'],
+};
