1592c33176b7481844e1fb6c27fd30e769f1b3f2
SergeyTokareff <sergey.v.tokareff@gmail.com>
2023-07-24 16:38:25 +0300

    fix bugs

--- src/components/layouts/Container/Container.jsx
+++ src/components/layouts/Container/Container.jsx
@@ -1,6 +1,11 @@
 import styles from './Container.module.css';
 
 // eslint-disable-next-line react/prop-types
-const Container = (props) => <div className={styles.container}>{props.children}</div>;
+const Container = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const classes = `${styles.container} ${props.className || ''}`;
+  // eslint-disable-next-line react/prop-types
+  return <div className={classes}>{props.children}</div>;
+};
 
 export default Container;
