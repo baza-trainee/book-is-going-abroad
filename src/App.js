43e691386071946dc26906109973bbbf59d4826e
Vakaliuk <69085662+Vakaliuk@users.noreply.github.com>
2023-07-30 21:33:28 +0300

    made first version of hero

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Footer from './components/layouts/Footer/Footer.jsx';
 import Header from './components/layouts/Header/Header.jsx';
 import Main from './components/layouts/Main/Main.jsx';
 import NotFoundPage from './components/layouts/NotFoundPage/NotFoundPage.jsx';
+import Hero from './components/layouts/Hero/Hero.jsx';
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Hero />
       <Routes>
         <Route path="/" element={<Main />} exact />
         <Route path="/*" element={<NotFoundPage />} exact />
