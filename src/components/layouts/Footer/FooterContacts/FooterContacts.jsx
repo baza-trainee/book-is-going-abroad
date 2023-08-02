3b201e37dad06a3f4d22dc4710e9b8f0741a40b3
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-08-02 09:11:42 +0300

    added functional to contacts

--- src/components/layouts/Footer/FooterContacts/FooterContacts.jsx
+++ src/components/layouts/Footer/FooterContacts/FooterContacts.jsx
@@ -1,27 +1,53 @@
-import FooterContactsItem from './FooterContactsItem/FooterContactsItem.jsx';
+import React, { useState, useEffect } from 'react';
+
+import CopyNotification from './index';
 
 import styles from './FooterContacts.module.css';
-import phone from './phone-icon.svg';
+import phoneNum from './phone-icon.svg';
 import email from './email-icon.svg';
 
 const contactsArray = [
-  { id: 1, text: '+38 044 209 53 02', icon: phone },
-  { id: 2, text: '+38 098 683 85 21', icon: phone },
-  { id: 3, text: '+38 098 306 84 84', icon: phone },
-  { id: 4, text: '+38 063 499 37 69', icon: phone }
+  { id: 1, phone: '+38 044 209 53 02', icon: phoneNum },
+  { id: 2, phone: '+38 098 683 85 21', icon: phoneNum },
+  { id: 3, phone: '+38 098 306 84 84', icon: phoneNum },
+  { id: 4, phone: '+38 063 499 37 69', icon: phoneNum }
 ];
 
 const emailLink = 'child.help.book@\ngmail.com';
 
 const FooterContacts = () => {
-  const RenderContacts = contactsArray.map(({ id, text, icon }) => (
-    <FooterContactsItem key={id} text={text} icon={icon} />
+  const [showCopyNotification, setShowCopyNotification] = useState(false);
+
+  const handleCopy = (phone) => {
+    navigator.clipboard.writeText(phone);
+    return setShowCopyNotification(true);
+  };
+
+  // eslint-disable-next-line consistent-return
+  useEffect(() => {
+    if (showCopyNotification) {
+      const timer = setTimeout(() => {
+        setShowCopyNotification(false);
+      }, 1250);
+
+      return () => clearTimeout(timer);
+    }
+  }, [showCopyNotification]);
+
+  const RenderContacts = contactsArray.map(({ id, phone, icon }) => (
+    <li key={id} icon={icon} onClick={() => handleCopy(phone)}>
+      <img src={icon} />
+      <p>{phone}</p>
+    </li>
   ));
 
   return (
     <div className={styles['footer-contacts']}>
+      {showCopyNotification && (
+        <CopyNotification message="Скопійовано!" />
+      )}
       <ul className={styles['footer-phones']}>{RenderContacts}</ul>
-      <div className={styles['footer-email']}>
+      <div className={styles['footer-email']} onClick={() => handleCopy(emailLink)}>
         <img src={email} />
         <a href="mailto:child.help.book@gmail.com">{emailLink}</a>
       </div>
