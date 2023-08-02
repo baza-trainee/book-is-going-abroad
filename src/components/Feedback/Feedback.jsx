7911320a73a56f794129f9a121406bd02e771b30
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-08-02 12:56:54 +0300

    add message - copy to phones

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,7 +1,8 @@
 import Container from '../layouts/Container/Container.jsx';
-import Button from '../UI/Button.jsx';
+import FeedbackForm from './FeedbackForm/FeedbackForm.jsx';
+import FeedbackPhones from './FeedbackPhones/FeedbackPhones.jsx';
+
 import styles from './Feedback.module.css';
-import icon from './feedback-phone-icon.svg';
 
 const Feedback = () => (
   <section className={styles.feedback}>
@@ -11,46 +12,11 @@ const Feedback = () => (
         <div className={styles['feedback-content-wrapper']}>
           <div className={styles['feedback-form-wrapper']}>
             <h4 className={styles['feedback-subtitle']}>Напишіть нам</h4>
-            <form className={styles['feedback-form']}>
-              <input
-                type="text"
-                name="text"
-                placeholder="Ім’я*"
-                required
-              />
-              <input
-                type="email"
-                name="email"
-                placeholder="Email*"
-                pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
-                required
-              />
-              <textarea type="text" placeholder="Повідомлення" />
-              <Button className={styles['feedback-button']} type="submit">
-                Надіслати
-              </Button>
-            </form>
+            <FeedbackForm />
           </div>
           <div className={styles['feedback-phones-wrapper']}>
             <h4 className={styles['feedback-subtitle']}>Зателефонуйте нам</h4>
-            <ul className={styles['phones-list']}>
-              <li>
-                <img src={icon} alt="phone-icon" />
-                <a href="tel:+380442095302">+38 044 209 53 02</a>
-              </li>
-              <li>
-                <img src={icon} alt="phone-icon" />
-                <a href="tel:+380986838521">+38 098 683 85 21</a>
-              </li>
-              <li>
-                <img src={icon} alt="phone-icon" />
-                <a href="tel:+380983068484">+38 098 306 84 84</a>
-              </li>
-              <li>
-                <img src={icon} alt="phone-icon" />
-                <a href="tel:+380634993769">+38 063 499 37 69</a>
-              </li>
-            </ul>
+            <FeedbackPhones />
           </div>
         </div>
       </div>
---
+++ src/components/Feedback/FeedbackPhones/FeedbackPhones.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import CopyNotification from './index';
+
+import styles from './FeedbackPhones.module.css';
+import phoneNum from './feedback-phone-icon.svg';
+
+const contactsArray = [
+  { id: 1, phone: '+38 044 209 53 02', icon: phoneNum },
+  { id: 2, phone: '+38 098 683 85 21', icon: phoneNum },
+  { id: 3, phone: '+38 098 306 84 84', icon: phoneNum },
+  { id: 4, phone: '+38 063 499 37 69', icon: phoneNum }
+];
+
+const FeedbackPhones = () => {
+  const [showCopyNotification, setShowCopyNotification] = useState(false);
+
+  const handleCopy = (phone) => {
+    navigator.clipboard.writeText(phone);
+    return setShowCopyNotification(true);
+  };
+
+  // eslint-disable-next-line consistent-return
+  useEffect(() => {
+    if (showCopyNotification) {
+      const timer = setTimeout(() => {
+        setShowCopyNotification(false);
+      }, 1250);
+
+      return () => clearTimeout(timer);
+    }
+  }, [showCopyNotification]);
+
+  const RenderContacts = contactsArray.map(({ id, phone, icon }) => (
+    <li key={id} icon={icon} onClick={() => handleCopy(phone)}>
+      <img src={icon} />
+      <p>{phone}</p>
+    </li>
+  ));
+
+  return (
+    <ul className={styles['phones-list']}>
+      {showCopyNotification && <CopyNotification message="Скопійовано!" />}
+      {RenderContacts}
+    </ul>
+  );
+};
+
+export default FeedbackPhones;
