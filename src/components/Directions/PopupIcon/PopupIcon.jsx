f94239e1a496db939e45293d54454131e4bc17c9
Vakaliuk <69085662+Vakaliuk@users.noreply.github.com>
2023-08-04 14:57:09 +0300

    fix

--- src/components/Directions/PopupIcon/PopupIcon.jsx
+++ src/components/Directions/PopupIcon/PopupIcon.jsx
@@ -4,7 +4,9 @@ import React from 'react';
 import styles from '../Directions.module.css';
 
 const PopupIcon = (props) => (
-  <div className={styles[`${'iconWrapper'}${props.row}`]}></div>
+  <div className={styles[`${'iconWrapper'}${props.row}`]}>
+    <img src={props.icon}></img>
+  </div>
 );
 
 export default PopupIcon;
