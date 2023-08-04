75215ffca760c9c15048c8b3076328bba30ce7cb
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-08-04 06:13:59 +0300

    fix styles tag <a>

--- src/components/layouts/Footer/FooterContacts/FooterContacts.jsx
+++ src/components/layouts/Footer/FooterContacts/FooterContacts.jsx
@@ -49,7 +49,7 @@ const FooterContacts = () => {
       <ul className={styles['footer-phones']}>{RenderContacts}</ul>
       <div className={styles['footer-email']} onClick={() => handleCopy(emailLink)}>
         <img src={email} />
-        <a href="mailto:child.help.book@gmail.com">{emailLink}</a>
+        <a className={styles['footer-email-link']} href="mailto:child.help.book@gmail.com">{emailLink}</a>
       </div>
     </div>
   );
