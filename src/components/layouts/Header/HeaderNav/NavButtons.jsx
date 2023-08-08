7f292f7015824d9c8e6367112dfdd0be76ab6f3a
Vakaliuk <69085662+Vakaliuk@users.noreply.github.com>
2023-08-08 20:01:09 +0300

    fixed header, done lang element, added link to nav button

--- src/components/layouts/Header/HeaderNav/NavButtons.jsx
+++ src/components/layouts/Header/HeaderNav/NavButtons.jsx
@@ -1,11 +1,20 @@
 /* eslint-disable react/prop-types */
+import React from 'react';
 import styles from '../Header.module.css';
 
-const NavButton = ({ icon, text }) => (
-  <button className={styles.navBtn}>
-    {icon}
-    <p className={styles.navBtnText}>{text}</p>
-  </button>
-);
+const NavButton = ({ icon, text, link }) => {
+  function scroll(className) {
+    const section = document.getElementsByClassName(className);
+    if (section.length > 0) {
+      section[0].scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+  return (
+    <button className={styles.navBtn} onClick={() => scroll(link)}>
+      {icon}
+      <p className={styles.navBtnText}>{text}</p>
+    </button>
+  );
+};
 
 export default NavButton;
