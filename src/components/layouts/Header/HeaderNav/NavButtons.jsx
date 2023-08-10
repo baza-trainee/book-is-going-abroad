25b69faf254f7179b42eaaa975dbd31dac8a8269
Vakaliuk <69085662+Vakaliuk@users.noreply.github.com>
2023-08-10 14:26:02 +0300

    add foo for go to main then scroll to sections

--- src/components/layouts/Header/HeaderNav/NavButtons.jsx
+++ src/components/layouts/Header/HeaderNav/NavButtons.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from '../Header.module.css';
 
 export function scroll(className, block) {
@@ -9,11 +10,34 @@ export function scroll(className, block) {
   }
 }
 
-const NavButton = ({ icon, text, link, block }) => (
-  <button className={styles.navBtn} onClick={() => scroll(link, block)}>
-    {icon}
-    <p className={styles.navBtnText}>{text}</p>
-  </button>
-);
+const NavButton = ({ icon, text, link, block }) => {
+  const navigate = useNavigate();
+  const [scrollAfterRedirect, setScrollAfterRedirect] = useState(false);
+
+  useEffect(() => {
+    if (scrollAfterRedirect) {
+      setTimeout(() => {
+        scroll(link, block);
+        setScrollAfterRedirect(false);
+      }, 100);
+    }
+  }, [scrollAfterRedirect, link, block]);
+
+  const changePathAndScroll = () => {
+    if (window.location.pathname === '/') {
+      scroll(link, block);
+    } else {
+      navigate('/#top');
+      setScrollAfterRedirect(true);
+    }
+  };
+
+  return (
+    <button className={styles.navBtn} onClick={changePathAndScroll}>
+      {icon}
+      <p className={styles.navBtnText}>{text}</p>
+    </button>
+  );
+};
 
 export default NavButton;
