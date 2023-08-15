872335bd84991fff134679001904d834d1beed94
SergeyTokareff <sergey.v.tokareff@gmail.com>
2023-08-15 17:57:50 +0300

    fix bugs

--- src/components/Activity/useWindowSize.js
+++ src/components/Activity/useWindowSize.js
@@ -3,14 +3,12 @@ import { useState, useEffect } from 'react';
 function useWindowSize() {
   const [windowSize, setWindowSize] = useState({
     width: window.innerWidth,
-    height: window.innerHeight,
   });
 
   useEffect(() => {
     const handleResize = () => {
       setWindowSize({
         width: window.innerWidth,
-        height: window.innerHeight,
       });
     };
 
