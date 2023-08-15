5c4b90372ea84709bcb55e5c0cf407d784b9c816
SergeyTokareff <sergey.v.tokareff@gmail.com>
2023-08-15 14:36:59 +0300

    first version adaptive features section

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -5,14 +5,18 @@ import Button from '../UI/Button.jsx';
 
 // eslint-disable-next-line import/no-unresolved
 import ideas from '../../assets/img/features/ideas-img.png';
+import ideasMobile from '../../assets/img/features/ideas-img-mobile.png';
 // eslint-disable-next-line import/no-unresolved
 import activity from '../../assets/img/features/activity-img.png';
+import activityMobile from '../../assets/img/features/activity-img-mobile.png';
+import arrow from './features-arrow.svg';
 import styles from './Features.module.css';
 
 const Features = () => {
   const [isTextExpanded, setIsTextExpanded] = useState(false);
   const textRef = useRef(null);
   const [textHeight, setTextHeight] = useState('10em');
+  const [arrowRotation, setArrowRotation] = useState(0);
 
   // eslint-disable-next-line max-len
   const text = 'Доступ кожної української дитини до книжок та підручників для навчання рідною мовою, незважаючи на місце їх тимчасового перебування. Надаючи кожній дитині книги та будь-яку літературу українською мовою, можемо зберегти ідентичність нашої нації за межами країни. Це особливо важливо для дітей, адже читання дає відчуття безпеки та стабільності у стресових ситуаціях. Маємо подбати про те, щоб українці у всіх куточках світу мали змогу зберігати свою культурну ідентичність, зокрема й навчати своїх дітей українською. Наша ціль забезпечити кожну дитину, що через війну була вимушена виїхати за кордон, власною книжкою, написаною рідною мовою. Для наших громадян доступ до українських книжок є надзвичайно важливою підтримкою у непростий воєнний час. Тому допомога дітям та їхнім родинам отримати частинку свого дому в іншій країні є надзвичайно важливою. Ми також пропонуємо будь-яку необхідну допомогу дітям-біженцям з України, бо саме вони є майбутнім нашої країни.';
@@ -27,6 +31,7 @@ const Features = () => {
 
   const handleButtonClick = () => {
     setIsTextExpanded((prevIsTextExpanded) => !prevIsTextExpanded);
+    setArrowRotation((prevRotation) => prevRotation + 180);
   };
 
   return (
@@ -43,9 +48,11 @@ const Features = () => {
             </div>
             <div
               className={`${styles['features-text-wrapper']} ${styles['features-text-top']}`}>
-              <h3 className={styles['features-title']}>
+              <h3 className={`${styles['features-title']} ${styles['features-title-top']}`}>
                 Головною ідеєю Організації є
               </h3>
+              <img src={ideasMobile} alt="Main ideas"
+              className={styles['features-img-mobile']} />
               <p className={styles['features-text']}>
                 Допомога постраждалим дітям-біженцям з України; надання допомоги
                 громадянам, постраждалим внаслідок правових режимів
@@ -59,6 +66,8 @@ const Features = () => {
               src={activity}
               alt="Activity goals"
             />
+            <img src={activityMobile} alt="Activity goals"
+              className={styles['features-img-mobile']} />
             <h3
               className={`${styles['features-title']} ${styles['features-title-float']}`}>
               Цілями діяльності Організації є
@@ -76,6 +85,7 @@ const Features = () => {
               }}
               className={styles['features-btn']}>
               {isTextExpanded ? 'Зменшити' : 'Збільшити'}
+              <img src={arrow} alt='Arrow' className={styles['features-arrow']} style={{ transform: `rotate(${arrowRotation}deg)` }} />
             </Button>
           </div>
         </div>
