4bc4f7f0d410115063bfec0a6ed9aa499d3f45a9
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-08-15 16:14:44 +0300

    added adaptation to support

--- src/components/layouts/Main/Main.jsx
+++ src/components/layouts/Main/Main.jsx
@@ -1,11 +1,11 @@
 // eslint-disable-next-line import/no-unresolved, import/extensions
-import Hero from '../../Hero/Hero.jsx';
-import Activity from '../../Activity/Activity.jsx';
-import Address from '../../Address/Address.jsx';
-import Features from '../../Features/Features.jsx';
-import Directions from '../../Directions/Directions.jsx';
-import Partners from '../../Partners/Partners.jsx';
-import Feedback from '../../Feedback/Feedback.jsx';
+// import Hero from '../../Hero/Hero.jsx';
+// import Activity from '../../Activity/Activity.jsx';
+// import Address from '../../Address/Address.jsx';
+// import Features from '../../Features/Features.jsx';
+// import Directions from '../../Directions/Directions.jsx';
+// import Partners from '../../Partners/Partners.jsx';
+// import Feedback from '../../Feedback/Feedback.jsx';
 // eslint-disable-next-line import/no-unresolved
 import styles from './Main.module.css';
 import Support from '../../Support/Support.jsx';
@@ -13,13 +13,13 @@ import Support from '../../Support/Support.jsx';
 const Main = () => (
   // eslint-disable-next-line react/prop-types
   <main className={styles.main}>
-    <Hero />
+    {/* <Hero />
     <Features />
     <Activity />
     <Address />
     <Directions />
     <Partners />
-    <Feedback />
+    <Feedback /> */}
     <Support />
   </main>
 );
