2c907afc5309d864515d4de43874b8516424992f
SergeyTokareff <sergey.v.tokareff@gmail.com>
2023-08-23 08:55:54 +0300

    add translate to activity section

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,7 +1,9 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, useContext } from 'react';
 
 import Container from '../layouts/Container/Container.jsx';
 import Button from '../UI/Button.jsx';
+// eslint-disable-next-line import/extensions
+import { TranslateContext } from '../../contexts/translate-context.js';
 
 // eslint-disable-next-line import/no-unresolved
 import ideas from '../../assets/img/features/ideas-img.png';
@@ -17,6 +19,8 @@ const Features = () => {
   const textRef = useRef(null);
   const [textHeight, setTextHeight] = useState('10em');
   const [arrowRotation, setArrowRotation] = useState(0);
+
+  const { translate } = useContext(TranslateContext);
 
   // eslint-disable-next-line max-len
   const text = 'Доступ кожної української дитини до книжок та підручників для навчання рідною мовою, незважаючи на місце їх тимчасового перебування. Надаючи кожній дитині книги та будь-яку літературу українською мовою, можемо зберегти ідентичність нашої нації за межами країни. Це особливо важливо для дітей, адже читання дає відчуття безпеки та стабільності у стресових ситуаціях. Маємо подбати про те, щоб українці у всіх куточках світу мали змогу зберігати свою культурну ідентичність, зокрема й навчати своїх дітей українською. Наша ціль забезпечити кожну дитину, що через війну була вимушена виїхати за кордон, власною книжкою, написаною рідною мовою. Для наших громадян доступ до українських книжок є надзвичайно важливою підтримкою у непростий воєнний час. Тому допомога дітям та їхнім родинам отримати частинку свого дому в іншій країні є надзвичайно важливою. Ми також пропонуємо будь-яку необхідну допомогу дітям-біженцям з України, бо саме вони є майбутнім нашої країни.';
@@ -84,7 +88,7 @@ const Features = () => {
                 handleButtonClick();
               }}
               className={styles['features-btn']}>
-              {isTextExpanded ? 'Зменшити' : 'Збільшити'}
+              {isTextExpanded ? translate('features.reduce') : translate('features.increase')}
               <img src={arrow} alt='Arrow' className={styles['features-arrow']} style={{ transform: `rotate(${arrowRotation}deg)` }} />
             </Button>
           </div>
