ad05a7e930d09609356c23a19b60950fef533d9c
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-08-30 10:42:59 +0300

    edit regex to name for Feedback form

--- src/components/Feedback/FeedbackForm/FeedbackForm.jsx
+++ src/components/Feedback/FeedbackForm/FeedbackForm.jsx
@@ -54,7 +54,8 @@ const FeedbackForm = () => {
     }
   }, [showSuccessSendMessage]);
 
-  const nameRegex = XRegExp('^(?!.*[%^*|~{};<>"`]).{2,30}$');
+  // const nameRegex = XRegExp('^(?!.*[%^*|~{};<>"`]).{2,30}$');
+  const nameRegex = XRegExp('^(?!.*[%^*|~{};<>"`0-9]).{2,30}$');
 
   const enteredNameIsValid = nameRegex.test(enteredName.trim());
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
